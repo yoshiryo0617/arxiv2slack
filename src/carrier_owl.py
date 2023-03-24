cc1789153d2bbf2c3ed4bfe886a5ab370a4bf4a8
agiats <taiga98-0809@g.ecc.u-tokyo.ac.jp>
2023-03-24 11:45:06 +0900

    no translation

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -1,22 +1,24 @@
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException
-import os
-import time
-import yaml
-import datetime
-import slackweb
 import argparse
+import datetime
+import os
 import textwrap
-from bs4 import BeautifulSoup
-import warnings
+import time
 import urllib.parse
+import warnings
 from dataclasses import dataclass
+
 import arxiv
 import requests
+import slackweb
+import yaml
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+
 # setting
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 
 @dataclass
@@ -40,34 +42,40 @@ def calc_score(abst: str, keywords: dict) -> (float, list):
     return sum_score, hit_kwd_list
 
 
-def search_keyword(
-        articles: list, keywords: dict, score_threshold: float
-        ) -> list:
+def search_keyword(articles: list, keywords: dict, score_threshold: float) -> list:
     results = []
-    
+
     # ヘッドレスモードでブラウザを起動
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless")
 
     # ブラウザーを起動
-    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
-    
+    driver = webdriver.Firefox(
+        executable_path=GeckoDriverManager().install(), options=options
+    )
+
     for article in articles:
-        url = article['arxiv_url']
-        title = article['title']
-        abstract = article['summary']
+        url = article["arxiv_url"]
+        title = article["title"]
+        abstract = article["summary"]
         score, hit_keywords = calc_score(abstract, keywords)
         if (score != 0) and (score >= score_threshold):
-            title_trans = get_translated_text('ja', 'en', title, driver)
-            abstract = abstract.replace('\n', '')
-            abstract_trans = get_translated_text('ja', 'en', abstract, driver)
+            # title_trans = get_translated_text("ja", "en", title, driver)
+            title_trans = title  # no translation needed
+            abstract = abstract.replace("\n", "")
+            # abstract_trans = get_translated_text('ja', 'en', abstract, driver)
+            abstract_trans = abstract  # no translation needed
             # abstract_trans = textwrap.wrap(abstract_trans, 40)  # 40行で改行
             # abstract_trans = '\n'.join(abstract_trans)
             result = Result(
-                    url=url, title=title_trans, abstract=abstract_trans,
-                    score=score, words=hit_keywords)
+                url=url,
+                title=title_trans,
+                abstract=abstract_trans,
+                score=score,
+                words=hit_keywords,
+            )
             results.append(result)
-    
+
     # ブラウザ停止
     driver.quit()
     return results
@@ -81,18 +89,18 @@ def send2app(text: str, slack_id: str, line_token: str) -> None:
 
     # line
     if line_token is not None:
-        line_notify_api = 'https://notify-api.line.me/api/notify'
-        headers = {'Authorization': f'Bearer {line_token}'}
-        data = {'message': f'message: {text}'}
+        line_notify_api = "https://notify-api.line.me/api/notify"
+        headers = {"Authorization": f"Bearer {line_token}"}
+        data = {"message": f"message: {text}"}
         requests.post(line_notify_api, headers=headers, data=data)
 
 
 def notify(results: list, slack_id: str, line_token: str) -> None:
     # 通知
-    star = '*'*80
+    star = "*" * 80
     today = datetime.date.today()
     n_articles = len(results)
-    text = f'{star}\n \t \t {today}\tnum of articles = {n_articles}\n{star}'
+    text = f"{star}\n \t \t {today}\tnum of articles = {n_articles}\n{star}"
     send2app(text, slack_id, line_token)
     # descending
     for result in sorted(results, reverse=True, key=lambda x: x.score):
@@ -102,21 +110,23 @@ def notify(results: list, slack_id: str, line_token: str) -> None:
         word = result.words
         score = result.score
 
-        text = f'\n score: `{score}`'\
-               f'\n hit keywords: `{word}`'\
-               f'\n url: {url}'\
-               f'\n title:    {title}'\
-               f'\n abstract:'\
-               f'\n \t {abstract}'\
-               f'\n {star}'
+        text = (
+            f"\n score: `{score}`"
+            f"\n hit keywords: `{word}`"
+            f"\n url: {url}"
+            f"\n title:    {title}"
+            f"\n abstract:"
+            f"\n \t {abstract}"
+            f"\n {star}"
+        )
 
         send2app(text, slack_id, line_token)
 
 
 def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) -> str:
-    '''
+    """
     https://qiita.com/fujino-fpu/items/e94d4ff9e7a5784b2987
-    '''
+    """
 
     sleep_time = 1
 
@@ -124,8 +134,14 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) ->
     from_text = urllib.parse.quote(from_text)
 
     # url作成
-    url = 'https://www.deepl.com/translator#' \
-        + from_lang + '/' + to_lang + '/' + from_text
+    url = (
+        "https://www.deepl.com/translator#"
+        + from_lang
+        + "/"
+        + to_lang
+        + "/"
+        + from_text
+    )
 
     driver.get(url)
     driver.implicitly_wait(10)  # 見つからないときは、10秒まで待つ
@@ -143,16 +159,18 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) ->
         return urllib.parse.unquote(from_text)
     return to_text
 
+
 def get_text_from_driver(driver) -> str:
     try:
-        elem = driver.find_element_by_class_name('lmt__translations_as_text__text_btn')
+        elem = driver.find_element_by_class_name("lmt__translations_as_text__text_btn")
     except NoSuchElementException as e:
         return None
-    text = elem.get_attribute('innerHTML')
+    text = elem.get_attribute("innerHTML")
     return text
 
+
 def get_text_from_page_source(html: str) -> str:
-    soup = BeautifulSoup(html, features='lxml')
+    soup = BeautifulSoup(html, features="lxml")
     target_elem = soup.find(class_="lmt__translations_as_text__text_btn")
     text = target_elem.text
     return text
@@ -161,8 +179,8 @@ def get_text_from_page_source(html: str) -> str:
 def get_config() -> dict:
     file_abs_path = os.path.abspath(__file__)
     file_dir = os.path.dirname(file_abs_path)
-    config_path = f'{file_dir}/../config.yaml'
-    with open(config_path, 'r') as yml:
+    config_path = f"{file_dir}/../config.yaml"
+    with open(config_path, "r") as yml:
         config = yaml.load(yml)
     return config
 
@@ -170,25 +188,26 @@ def get_config() -> dict:
 def main():
     # debug用
     parser = argparse.ArgumentParser()
-    parser.add_argument('--slack_id', default=None)
-    parser.add_argument('--line_token', default=None)
+    parser.add_argument("--slack_id", default=None)
+    parser.add_argument("--line_token", default=None)
     args = parser.parse_args()
 
     config = get_config()
-    subject = config['subject']
-    keywords = config['keywords']
-    score_threshold = float(config['score_threshold'])
+    subject = config["subject"]
+    keywords = config["keywords"]
+    score_threshold = float(config["score_threshold"])
 
     day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
-    day_before_yesterday_str = day_before_yesterday.strftime('%Y%m%d')
+    day_before_yesterday_str = day_before_yesterday.strftime("%Y%m%d")
     # datetime format YYYYMMDDHHMMSS
-    arxiv_query = f'({subject}) AND ' \
-                  f'submittedDate:' \
-                  f'[{day_before_yesterday_str}000000 TO {day_before_yesterday_str}235959]'
-    articles = arxiv.query(query=arxiv_query,
-                           max_results=1000,
-                           sort_by='submittedDate',
-                           iterative=False)
+    arxiv_query = (
+        f"({subject}) AND "
+        f"submittedDate:"
+        f"[{day_before_yesterday_str}000000 TO {day_before_yesterday_str}235959]"
+    )
+    articles = arxiv.query(
+        query=arxiv_query, max_results=1000, sort_by="submittedDate", iterative=False
+    )
     results = search_keyword(articles, keywords, score_threshold)
 
     slack_id = os.getenv("SLACK_ID") or args.slack_id
